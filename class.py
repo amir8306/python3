b243eb496b339b5be5456a74e5b88440e8061d0b
amir8306 <amir830685@gmail.com>
2021-05-02 13:30:06 +0430

    class

---
+++ class.py
@@ -0,0 +1,8 @@
+ave = [18 , 16 ,19.5]
+math = [14 ,18 , 20]
+for i in range(3):
+    a = ave + math
+    int(input('please enter your mark ? ...> '))
+    if 19.5 or 20 == a:
+        print('your are Register')
+    
