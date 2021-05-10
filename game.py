14e59210c6e81e089b1d4b51a6922e0410bff133
amir8306 <amir830685@gmail.com>
2021-05-10 13:24:48 +0430

    game

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+#game with numbers
 print("hello welcome to play")
 a = int(input())
 b = int(input())
@@ -11,3 +12,20 @@ for i in range(20):
         a = c
     elif result == 2 :
         b = c
+print("------------------- first one finished -------------------")
+
+#finding even numbers in a list
+list2 = []
+for i in range(10):
+    list1 = int(input('please Enter your nauber ...> '))
+    list2.append(list1)
+
+for x in list2:
+    if x % 2 == 0:
+        print(x)
+
+print("------------------- second one finished -------------------")
+#finding largest number in a list
+red = [20 ,57 , 43 , 91 ,751 , 21 ,43]
+red.sort()
+print(red[-1])
