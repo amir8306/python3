dce02dcbdfaad430a0e15afbc3fe622acace759b
amir8306 <amir830685@gmail.com>
2021-08-07 15:13:40 +0430

    class1

--- class.py
+++ class.py
@@ -1,8 +1,24 @@
-ave = [18 , 16 ,19.5]
-math = [14 ,18 , 20]
-for i in range(3):
-    a = ave + math
-    int(input('please enter your mark ? ...> '))
-    if 19.5 or 20 == a:
-        print('your are Register')
+class school:
+    def __init__(self ,  name , last_name , average , teachre , fild ):
+        self.name1 = name
+        self.last_name1 = last_name
+        self.average1 = average
+        self.teachre1 = teachre
+        self.fild1 = fild
     
+    
+    def fild(self):
+        if self.fild1 == "math":
+            print("red")
+        elif self.fild1 == "Science":
+            print("blue")
+        elif self.fild1 == "human":
+            print("brown")
+
+    def infor(self):
+        print(self.name1)
+
+
+a = school('amir' , 'ahmadi' , 18 , 'ali' , 'math')
+print(a.fild())
+print(a.infor())
